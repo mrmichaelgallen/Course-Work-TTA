acbf8c5b6e9cc7b2bf524e0b555156240cad99ca
mrmichaelgallen <mr.michael.g.allen@gmail.com>
2016-02-24 09:23:39 -0800

    update

--- Python/Python3_Tutorial/Python3_Tutorial_Conditionals.py
+++ Python/Python3_Tutorial/Python3_Tutorial_Conditionals.py
@@ -1956,22 +1956,109 @@
 
 # how to write images and binary files
 
-def main():
-    buffersize = 50000
-    infile = open('olives.jpg', 'rb') # rb = read binary
-    outfile - open('new.jpg', 'wb') # wb = write binary
-    buffer = infile.read(buffersize) # .read(buffersize) is not iterable
-    while len(buffer):
-        outfile.write(buffer)
-        print('.', end = '')
-        buffer = infile.read(buffer)
-    print()
-    print('Done.')
-
-if __name__ == "__main__": main()
+# def main():
+#     buffersize = 50000
+#     infile = open('olives.jpg', 'rb') # rb = read binary
+#     outfile - open('new.jpg', 'wb') # wb = write binary
+#     buffer = infile.read(buffersize) # .read(buffersize) is not iterable
+#     while len(buffer):
+#         outfile.write(buffer)
+#         print('.', end = '')
+#         buffer = infile.read(buffer)
+#     print()
+#     print('Done.')
+#
+# if __name__ == "__main__": main()
 
 ##
 ## 16. Database
+##
+
+##
+## Creating a database with SQLite3
+##
+
+# SQLite3 is built into Python 3, It is better than MySQL. SQLite is serverless, doesnt require other engines and fully transactional
+
+# import sqlite3
+#
+# def main():
+#     db = sqlite3.connect('test.db')
+#     db.row_factory = sqlite3.Row  # Makes the output row objects
+#     db.execute('drop table if exists test')
+#     db.execute('create table test (t1 text, i1 int)')
+#     db.execute('insert into test (t1, i1) values (?,?)', ('one',1))
+#     db.execute('insert into test (t1, i1) values (?,?)', ('two',2))
+#     db.execute('insert into test (t1, i1) values (?,?)', ('three',3))
+#     db.execute('insert into test (t1, i1) values (?,?)', ('four',4))
+#     db.commit()
+#     cursor = db.execute('select * from test order by t1')
+#     for row in cursor:
+#         # print(row) # Data returned in tuples
+#         # print(dict(row)) # As dictionaries instead of row objects
+#         # print(row['t1']) # shows the column t1
+#         print(row['t1'], row['i1']) # gets all the data from this particular database
+#
+# if __name__ == "__main__": main()
+
+##
+## Creating, retrieving, updating and deleting records
+##
+
+# import sqlite3
+#
+# def insert(db, row):
+#     db.execute('insert into test (t1, i1) values (?, ?)', (row['t1'], row['i1']))
+#     db.commit()
+#
+# def retrieve(db, t1):
+#     cursor = db.execute('select * from test where t1 = ?', (t1,))
+#     return cursor.fetchone()
+#
+# def update(db, row):
+#     db.execute('update test set i1 = ? where t1 = ?', (row['i1'], row['t1']))
+#     db.commit()
+#
+# def delete(db, t1):
+#     db.execute('delete from test where t1 = ?', (t1,))
+#     db.commit()
+#
+# def disp_rows(db):
+#     cursor = db.execute('select * from test order by t1')
+#     for row in cursor:
+#         print('  {}: {}'.format(row['t1'], row['i1']))
+#
+# def main():
+#     db = sqlite3.connect('test.db')
+#     db.row_factory = sqlite3.Row
+#     print('Create table test')
+#     db.execute('drop table if exists test')
+#     db.execute('create table test ( t1 text, i1 int )')
+#
+#     print('Create rows')
+#     insert(db, dict(t1 = 'one', i1 = 1))
+#     insert(db, dict(t1 = 'two', i1 = 2))
+#     insert(db, dict(t1 = 'three', i1 = 3))
+#     insert(db, dict(t1 = 'four', i1 = 4))
+#     disp_rows(db)
+#
+#     print('Retrieve rows')
+#     print(dict(retrieve(db, 'one')), dict(retrieve(db, 'two')))
+#
+#     print('Update rows')
+#     update(db, dict(t1 = 'one', i1 = 101))
+#     update(db, dict(t1 = 'three', i1 = 103))
+#     disp_rows(db)
+#
+#     print('Delete rows')
+#     delete(db, 'one')
+#     delete(db, 'three')
+#     disp_rows(db)
+#
+# if __name__ == "__main__": main()
+
+##
+## Creating a database object
 ##
 
 
