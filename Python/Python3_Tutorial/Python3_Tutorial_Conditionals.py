18c1a081bb2a68307277d7c46c4a7c3d439933d2
mrmichaelgallen <mr.michael.g.allen@gmail.com>
2016-02-25 09:28:50 -0800

    updating

--- Python/Python3_Tutorial/Python3_Tutorial_Conditionals.py
+++ Python/Python3_Tutorial/Python3_Tutorial_Conditionals.py
@@ -2061,6 +2061,174 @@
 ## Creating a database object
 ##
 
+# import sqlite3
+#
+# class database:
+#     def __init__(self, **kwargs):
+#         self.filename = kwargs.get('filename')
+#         self.table = kwargs.get('table', 'test')
+#
+#     def sql_do(self, sql, *params):
+#         self._db.execute(sql, params)
+#         self._db.commit()
+#
+#     def insert(self, row):
+#         self._db.execute('insert into {} (t1, i1) values (?, ?)'.format(self._table), (row['t1'], row['i1']))
+#         self._db.commit()
+#
+#     def retrieve(self, key):
+#         cursor = self._db.execute('select * from {} where t1 = ?'.format(self._table), (key,))
+#         return dict(cursor.fetchone())
+#
+#     def update(self, row):
+#         self._db.execute(
+#             'update {} set i1 = ? where t1 = ?'.format(self._table),
+#             (row['i1'], row['t1']))
+#         self._db.commit()
+#
+#     def delete(self, key):
+#         self._db.execute('delete from {} where t1 = ?'.format(self._table), (key,))
+#         self._db.commit()
+#
+#     def disp_rows(self):
+#         cursor = self._db.execute('select * from {} order by t1'.format(self._table))
+#         for row in cursor:
+#             print('  {}: {}'.format(row['t1'], row['i1']))
+#
+#     def __iter__(self):
+#         cursor = self._db.execute('select * from {} order by t1'.format(self._table))
+#         for row in cursor:
+#             yield dict(row)
+#
+#     @property
+#     def filename(self): return self._filename
+#
+#     @filename.setter
+#     def filename(self, fn):
+#         self._filename = fn
+#         self._db = sqlite3.connect(fn)
+#         self._db.row_factory = sqlite3.Row
+#
+#     @filename.deleter
+#     def filename(self): self.close()
+#
+#     @property
+#     def table(self): return self._table
+#     @table.setter
+#     def table(self, t): self._table = t
+#     @table.deleter
+#     def table(self): self._table = 'test'
+#
+#     def close(self):
+#             self._db.close()
+#             del self._filename
+#
+# def main():
+#     db = database(filename = 'test.db', table = 'test') # Initialize database
+#
+#     print('Create table test')
+#     db.sql_do('drop table if exists test')
+#     db.sql_do('create table test ( t1 text, i1 int )')
+#
+#     print('Create rows')
+#     db.insert(dict(t1 = 'one', i1 = 1))
+#     db.insert(dict(t1 = 'two', i1 = 2))
+#     db.insert(dict(t1 = 'three', i1 = 3))
+#     db.insert(dict(t1 = 'four', i1 = 4))
+#     for row in db: print(row)
+#
+#     print('Retrieve rows')
+#     print(db.retrieve('one'), db.retrieve('two'))
+#
+#     print('Update rows')
+#     db.update(dict(t1 = 'one', i1 = 101))
+#     db.update(dict(t1 = 'three', i1 = 103))
+#     for row in db: print(row)
+#
+#     print('Delete rows')
+#     db.delete('one')
+#     db.delete('three')
+#     for row in db: print(row)
+#
+# if __name__ == "__main__": main()
+
+##
+## 17. Modules
+##
+
+##
+## Using standard library modules
+##
+
+# Modules for adding functionality to your programs
+# Go through list to get familiar with list
+
+# import sys
+#
+# def main():
+#     print('Python version {}.{}.{}'.format(*sys.version_info))
+#     print(sys.platform)
+#     print()
+#
+#     # import os # Python allows you to import modules selectively within function
+#     # print(os.name) # Gets the current type of os
+#     # print(os.getenv('PATH')) # Gets the path of the ox
+#     # print(os.getcwd()) # CWD = Current working directory
+#     # print(os.urandom(25))
+#
+#     # import urllib.request
+#     # page = urllib.request.urlopen('http://bw.org/')
+#     # # print(page) # gives the object for that webpage
+#     # for line in page: print(str(line, encoding = 'UTF_8'), end = '')
+#
+#     import random
+#     # print(random.randint(1, 1000))
+#
+#     # x = list(range(25))
+#     # print(x)
+#     # random.shuffle(x)
+#     # print(x)
+#
+#     import datetime
+#     now = datetime.datetime.now()
+#     print(now)
+#     print(now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond)
+#
+#
+#
+# if __name__ == "__main__": main()
+
+##
+## Finding third-party modules    #http://www.lynda.com/Python-tutorials/Finding-third-party-modules/62226/71013-4.html
+##
+
+# PyPI is http://pypi.python.org/pypi  PyPI = Python Package Index
+# All PyPI modules on site have the same setup instructions
+
+# import bitstring
+#
+# def main():
+#     a = bitstring.BitString(bin = '01010101')
+#     print(a.hex, a.bin, a.uint)
+#
+# if __name__ == "__main__": main()
+
+##
+## Creating your own module # http://www.lynda.com/Python-tutorials/Creating-module/62226/71014-4.html
+##
+
+# When creating your own module, make sure not to have any code outside of a class or function, which is a good practice anyways
+
+##
+## 18. Debugging
+##
+
+##
+## Dealing with syntax errors
+##
+
+
+
 
 
 ###
