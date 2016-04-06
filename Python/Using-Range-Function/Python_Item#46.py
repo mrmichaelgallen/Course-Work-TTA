3dc31214ccd5a3a5f30b6a48761ba568bbdf0973
mrmichaelgallen <mr.michael.g.allen@gmail.com>
2016-04-05 18:04:32 -0700

    Adding Drills

---
+++ Python/Using-Range-Function/Python_Item#46.py
@@ -0,0 +1,23 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:54:25) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> for item in list(range(4)): print(item)
+
+0
+1
+2
+3
+>>> for item in list(range(3, -1, -3)) print(item)
+SyntaxError: invalid syntax
+>>> for item in list(range(3, -1, -1)): print(item)
+
+3
+2
+1
+0
+>>> for item in list(range(8, 0, -2)): print(item)
+
+8
+6
+4
+2
+>>> 
